0e83ffd8ef5c7d30986e8565c4ccd9751eb8c5c7
Mahesh zarekar <Mahesh zarekar@LAPTOP-F4VOD12N>
2020-12-11 10:04:15 +0530

    Adding Login.java

---
+++ HelloWorldServlet/src/com/csi/serverside/Login.java
@@ -0,0 +1,10 @@
+package com.csi.serverside;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Login...");
+	}
+
+}
